a46a354954aabd54e2f35561cf4abc03a47d15c4
gasing-tambah <adnan.b@gmail.com>
2026-06-03 17:02:49 +0700

    fix(php7): Remove mixed type hint for PHP 7 compatibility

--- helpers/response.php
+++ helpers/response.php
@@ -1,6 +1,6 @@
 <?php
 
-function json_response(mixed $data, int $code = 200): void
+function json_response($data, int $code = 200): void
 {
     http_response_code($code);
     header('Content-Type: application/json');
@@ -13,7 +13,7 @@ function error_response(string $message, int $code = 400): void
     json_response(['error' => $message], $code);
 }
 
-function success_response(mixed $data = null, string $message = 'OK', int $code = 200): void
+function success_response($data = null, string $message = 'OK', int $code = 200): void
 {
     $res = ['message' => $message];
     if ($data !== null) {
